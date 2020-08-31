1788a35f89d7911458b7c13ac7bb6cb6ab02e5a9
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-08-31 14:32:18 -0700

    created login form, non funtional

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -15,10 +15,40 @@ export default class LoginForm extends Component {
   }
 
   render() {
+    const {error} = this.state;
     return (
-      <div>
-        
-      </div>
+      <form className='LoginForm'
+        onSubmit={this.handleSubmitJwtAuth}
+      >
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
+        <div className='user_name'>
+          <label htmlFor='LoginForm__user_name'>
+            User name
+          </label>
+          <Input
+            name='user_name'
+            type='text'
+            required
+            id='LoginForm__user_name'
+          />
+        </div>
+        <div className='password'>
+          <label htmlFor='LoginForm__password'>
+            Password
+          </label>
+          <Input
+            name='password'
+            type='password'
+            required
+            id='LoginForm__password'
+          />
+        </div>
+        <Button type='submit'>
+          Login
+        </Button>
+      </form>
     )
   }
 };
