4104bc0bfedc86f405848847e46c10526aaba563
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-09-10 08:04:54 -0700

    Fixed API_ENDPOINT in config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://warm-harbor-99058.herokuapp.com',
+  API_ENDPOINT: 'https://warm-harbor-99058.herokuapp.com/api',
   TOKEN_KEY: process.env.REACT_APP_API_KEY
 };
