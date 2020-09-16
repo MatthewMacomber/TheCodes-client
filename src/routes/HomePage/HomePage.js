0017ef84b8854b54762c5b5b70decd03944a1028
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-09-16 07:54:54 -0700

    Changes to styling

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -5,11 +5,16 @@ import './HomePage.css';
 
 export default class HomePage extends Component {
   state = {
-    homeImage: ''
+    homeImage: '',
+    homeAltImage: '',
   };
 
   setHomeImage = homeImage => {
     this.setState({homeImage})
+  }
+
+  setHomeAltImage = homeAltImage => {
+    this.setState({homeAltImage})
   }
   
   componentDidMount() {
@@ -18,18 +23,41 @@ export default class HomePage extends Component {
       fontSize: 50,
       lineHeight: 55,
       textAlign: 'center',
-      bgColor: '#000000AA',
+      bgColor: '#00000020',
       textColor: 'red'
     })
       .then(this.setHomeImage);
+    
+    const homeAltText = 'Solve codes, win prizes, prove just how vast your knowledge is in cracking codes and ciphers.'
+    textImage.generate(homeAltText, {
+      fontSize: 50,
+      lineHeight: 55,
+      textAlign: 'center',
+      bgColor: '#00000020',
+      textColor: 'red'
+    })
+      .then(this.setHomeAltImage)
   };
+
+  onMouseOver = e => {
+    e.target.src = this.state.homeAltImage;
+  }
+
+  onMouseOut = e => {
+    e.target.src = this.state.homeImage;
+  }
 
   render() {
     let homeImage = this.state.homeImage;
     return (
       <div>
         <Section className='main-img'>
-          <img src={homeImage} alt="Homepage example code using engima machine: jibyr mmuwo cxtvg kozre pdizu hrgre dmyhx fvrfm ffdov yrcgx ymqlp dsxys vsjsb sbzbl wkklx"/>
+          <img
+            src={homeImage}
+            onMouseOver={this.onMouseOver}
+            onMouseOut={this.onMouseOut}
+            alt="Homepage example code: jibyr mmuwo cxtvg kozre pdizu hrgre dmyhx fvrfm ffdov yrcgx ymqlp dsxys vsjsb sbzbl wkklx"
+          />
         </Section>
         <Section>
           <hr/>{/* TODO Add login and register buttons that only appear if a user is not logged in. */}
