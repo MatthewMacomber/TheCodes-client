cbc7ff52aeed23f9897e0f90e8e7206447c8f119
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-09-29 10:29:39 -0700

    Added some limited testing

---
+++ src/components/CodeListItem/CodeListItem.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import CodeListItem from './CodeListItem';
+
+describe('CodeListItem component', () => {
+  //const props = {}
+});
